26faba9d119066052500f7fcbc5a1b7d526156af
akimegane0730 <akimegane0730o@gmail.com>
2020-04-24 21:40:45 +0900

    index&new-page

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,8 @@ Route::get('/', function () {
 
 Route::get('/shops', 'ShopController@index')->name('shop.list');
 Route::get('/shops/new', 'ShopController@create')->name('shop.new');
+Route::post('/shop', 'ShopController@store')->name('shop.store');
+Route::get('/shop/edit/{id}', 'ShopController@edit')->name('shop.edit');
+Route::post('/shop/update/{id}', 'ShopController@update')->name('shop.update');
 Route::get('/shops/{id}', 'ShopController@show')->name('shop.detail');
+Route::delete('/shop/{id}', 'ShopController@destroy')->name('shop.destroy');
