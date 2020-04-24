9200db0f24f24575164d8f85b9c50e77cfd2e0ef
akimegane0730 <akimegane0730o@gmail.com>
2020-04-24 12:47:35 +0900

    create index-page

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/shops');
 });
+
+Route::get('/shops', 'ShopController@index')->name('shop.list');
