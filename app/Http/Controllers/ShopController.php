23ed578714ec0c29f11483a2ef7ee03e28b73013
akimegane0730 <akimegane0730o@gmail.com>
2020-04-24 23:33:46 +0900

    authの制御

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -8,6 +8,10 @@ use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
+    public function __contruct()
+    {
+        $this->middleware('auth')->except(['index', 'show']);
+    }
     /**
      * Display a listing of the resource.
      *
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,8 +19,9 @@
             </tr>
         @endforeach
     </table>
-
-    <div>
-        <a href={{ route('shop.new') }} class="btn btn-outline-primary">新しいお店</a>
-    </div>
+    @auth
+        <div>
+            <a href={{ route('shop.new') }} class="btn btn-outline-primary">新しいお店</a>
+        </div>
+    @endauth
 @endsection
