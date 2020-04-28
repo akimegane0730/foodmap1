72e14d8f5d34c7cfc3a2f85c1e985cb640f74fee
akimegane0730 <akimegane0730o@gmail.com>
2020-04-28 21:12:52 +0900

    edit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,7 +23,9 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    use Illuminate\Support\Facades\Schema;
+
+    
+
     public function boot()
     {
         Schema::defaultStringLength(191);
